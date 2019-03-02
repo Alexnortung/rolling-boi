d0216dc5acde48722187bdebb1119caa554325b3
Alexander Nortung <alex_nortung@live.dk>
2019-03-02 20:22:32 +0100

    blobblob

--- Assets/Tilemaps/tileset.tsx
+++ Assets/Tilemaps/tileset.tsx
@@ -1,10 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="tileset" tilewidth="1484" tileheight="142" tilecount="2" columns="0">
+<tileset version="1.2" tiledversion="1.2.2" name="tileset" tilewidth="1484" tileheight="359" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="166" height="142" source="../sprites/defaultwall.png"/>
  </tile>
  <tile id="1">
   <image width="1484" height="104" source="../sprites/path.png"/>
+ </tile>
+ <tile id="3">
+  <image width="139" height="359" source="../sprites/Alien stråle NY.png"/>
+ </tile>
+ <tile id="4">
+  <image width="317" height="247" source="../sprites/Anti Gravity NY.png"/>
+ </tile>
+ <tile id="5">
+  <image width="76" height="69" source="../sprites/enemy.png"/>
+ </tile>
+ <tile id="6">
+  <image width="418" height="209" source="../sprites/pigge NY.png"/>
+ </tile>
+ <tile id="7">
+  <image width="323" height="259" source="../sprites/Reverse NY.png"/>
+ </tile>
+ <tile id="8">
+  <image width="297" height="162" source="../sprites/hoppedude NY.png"/>
+ </tile>
+ <tile id="9">
+  <image width="71" height="59" source="../sprites/main-player.png"/>
+ </tile>
+ <tile id="10">
+  <image width="225" height="232" source="../sprites/finish-Goal.png"/>
  </tile>
 </tileset>
