bf6348ff5bae1552bf8ab9cfa9d545d0b6390842
Alexander Nortung <alex_nortung@live.dk>
2019-03-02 17:40:49 +0100

    tilemaps

---
+++ Assets/Tilemaps/tileset.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="tileset" tilewidth="1484" tileheight="142" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="166" height="142" source="../sprites/defaultwall.png"/>
+ </tile>
+ <tile id="1">
+  <image width="1484" height="104" source="../sprites/path.png"/>
+ </tile>
+</tileset>
