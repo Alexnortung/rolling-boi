c4f2d416263d41308444c628c8fde7aa44f69f77
Alexnortung <alex_nortung@live.dk>
2019-03-03 21:41:43 +0100

    endscreen finished

---
+++ Assets/scripts/UI/imageScaler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class imageScaler : MonoBehaviour {
+
+    public Camera camera;
+    public Image image;
+
+	// Use this for initialization
+	void Start () {
+        float cw = camera.pixelWidth;
+        float ch = camera.pixelHeight;
+        float iw = image.rectTransform.rect.xMax - image.rectTransform.rect.xMin;
+        float ih = image.rectTransform.rect.yMax - image.rectTransform.rect.yMin;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
