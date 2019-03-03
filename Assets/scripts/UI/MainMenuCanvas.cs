c96bd935aecb71989f099e05aaa1fb909058b16f
Alexnortung <alex_nortung@live.dk>
2019-03-03 19:10:15 +0100

    created gamemanagercreator

---
+++ Assets/scripts/UI/MainMenuCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuCanvas : MonoBehaviour {
+
+    public int levelInt;
+
+    private Button buttonComp;
+    private ManageGame manageGame;
+
+	// Use this for initialization
+	void Start () {
+        manageGame = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ManageGame>();
+        buttonComp = gameObject.GetComponent<Button>();
+        buttonComp.onClick.AddListener(delegate { manageGame.SelectLevel(levelInt); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/gameManager/gamemanagerCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanagerCreator : MonoBehaviour {
+
+    public GameObject manageGame;
+
+	// Use this for initialization
+	void Awake () {
+        GameObject other = GameObject.FindGameObjectWithTag("GameManager");
+        if (other == null) Instantiate(manageGame);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
